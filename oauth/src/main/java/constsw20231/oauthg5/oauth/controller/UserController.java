86b95b3b2fa07b336b6ef7f978c6a37a76379df5
henriquezanette <henrique.zanette01@edu.pucrs.br>
2023-04-10 19:25:23 -0300

    add controller

---
+++ oauth/src/main/java/constsw20231/oauthg5/oauth/controller/UserController.java
@@ -0,0 +1,10 @@
+package constsw20231.oauthg5.oauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class UserController {
+
+}
