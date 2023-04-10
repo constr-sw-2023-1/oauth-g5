ffa766845efbf0def8c15744bdd24ab997e38ad0
henriquezanette <henrique.zanette01@edu.pucrs.br>
2023-04-10 19:13:31 -0300

    remove unused imports

--- oauth/src/main/java/constsw20231/oauthg5/oauth/OauthApplication.java
+++ oauth/src/main/java/constsw20231/oauthg5/oauth/OauthApplication.java
@@ -1,11 +1,8 @@
 package constsw20231.oauthg5.oauth;
 
-import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignErrorDecoderFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
